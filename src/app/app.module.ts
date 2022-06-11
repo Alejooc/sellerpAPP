2d11bc6b824210acddbd4a2461ff20940a7be4ab
Alejandro Zapata <programador2@aristextil.com>
2022-06-11 09:09:17 -0500

    'cambios'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,ReactiveFormsModule,IonicStorageModule.forRoot({
     name: '__mydb'
-  })],
+  }), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
   schemas: [
