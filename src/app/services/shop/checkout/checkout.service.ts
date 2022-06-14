b4fdea4635baded8ebed1bce55e8d0176ecb07aa
alejosorio <engomadoonpixeles@gmail.com>
2022-06-14 15:38:42 -0500

    'checkout'

--- src/app/services/shop/checkout/checkout.service.ts
+++ src/app/services/shop/checkout/checkout.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { AppSettings } from '../../../config/config';
+import { StorageService } from "../../storage/storage.service";
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+  public car=[];
+  public AppSettings = new AppSettings;
+  constructor(public http: HttpClient,private stora:StorageService) {
+    this.cart();
+   }
 
-  constructor() { }
+  /*getPaymet(city:string):Observable<HttpResponse<any>>{
+    return this.http.get<any>(`${this.AppSettings.API}get_PayMeth/`,{ observe: 'response' });
+  }*/
+  getPaymet(city:string):Observable<any>{
+    const _urlParams: any = new FormData();
+    _urlParams.append('city',JSON.stringify(city));
+    return this.http.post(`${this.AppSettings.API}get_PayMeth`,_urlParams);
+  }
+  getAddress(user:number):Observable<any>{
+    const _urlParams: any = new FormData();
+    _urlParams.append('user',JSON.stringify(user));
+    return this.http.post(`${this.AppSettings.API}get_addressU`,_urlParams);
+  }
+  async cart(): Promise<void>{
+    let f = await this.stora.get('cart');
+    console.log(f);
+    this.car =f;
+  }
+  sendOrder(order:any):Observable<any>{
+    const _urlParams: any = new FormData();
+    _urlParams.append('payform',JSON.stringify(order));
+    _urlParams.append('prods',JSON.stringify(this.car));
+    return this.http.post(`${this.AppSettings.API}send_order`,_urlParams);
+  }
 }
--- src/app/shop/checkout/checkout/checkout.page.ts
+++ src/app/shop/checkout/checkout/checkout.page.ts
@@ -2,51 +2,110 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StorageService} from "../../../services/storage/storage.service";
 import { AuthService } from "../../../services/auth/auth.service";
+import { ProfileService } from "../../../services/shop/profile/profile.service";
+import {ActivatedRoute,Router } from '@angular/router';
+import { CheckoutService } from "../../../services/shop/checkout/checkout.service";
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.page.html',
   styleUrls: ['./checkout.page.scss'],
 })
 export class CheckoutPage implements OnInit {
-  loginForm:FormGroup;
+  checkoutForm:FormGroup;
   activeDes:boolean=false;
   total:number=0;
   perdesc:any;
   perCurrenc:any;
   totalDesc:number=0;
+  login:boolean=true;
+  cit: any[];
+  citys: any;
+  states: any;
+  payme: any[];
+  user: any;
+  myAddress:any[];
+  cart: any;
+  logg: any;
+  ctotal:any;
   constructor(private formBuilder:FormBuilder,
     private storage:StorageService,
-    private auth:AuthService) { }
+    private auth:AuthService,
+    private servicePro:ProfileService,
+    private checkServide:CheckoutService,
+    route:ActivatedRoute) { 
+      route.params.subscribe(async val => {
+        const data=  await this.storage.get('userData') || [];
+        this.ctotal = await this.storage.getTotalCart('cart');
+        if (data.length==0) {
+          this.login=false;
+          this.logg=0;
+        }else{
+          this.checkServide.getAddress(data.decode.info.id).subscribe(resp=>{
+            console.log(resp);
+            this.myAddress= resp.data;
+            this.storage.set('address',this.myAddress[0]);
+            this.logg=1;
+          })
+        }
+      
+      });
+    }
 
 
-    ngOnInit() {
-      this.loginForm= this.formBuilder.group(
+    ngOnInit() {     
+      this.servicePro.getStates().subscribe(resp=>{
+        this.states=resp.body.states;
+        this.citys=resp.body.citys;
+      })
+      this.cart =  this.storage.get('cart') || [];
+
+      this.checkoutForm= this.formBuilder.group(
         {
-          user:["",[Validators.required,Validators.minLength(5)]],
-          password:["",[Validators.required,Validators.minLength(4)]]
+          name:["",[Validators.required]],
+          doc:["",[Validators.required]],
+          email:[""],
+          address:["",[Validators.required]],
+          phone:["",[Validators.required]],
+          dep:["",[Validators.required]],
+          city:["",[Validators.required]],
+          barrio:["",[Validators.required]],
+          payme:["",[Validators.required]],
+          isLogged:[this.logg],
+          total:[this.ctotal]
         }
       )
     }
     submitForm(){
-      if (this.loginForm.invalid) {
+      if (this.checkoutForm.invalid) {
         console.warn('empty form');
-  
+        
       }else{
-        this.auth.login(this.loginForm.value).subscribe(resp=>{
+        this.checkServide.sendOrder(this.checkoutForm.value).subscribe(resp=>{
           console.log(resp);
-          if (resp.tipo ==1) {
-            console.log(resp.msg);
-            this.storage.set('userData',{token:resp.info,decode:this.auth.getDecodedAccessToken(resp.info)});
-           // this.router.navigate(['/']);
-            
-          }else{
-            console.log(resp.msg);
-          }
-          
-    
         })
       }
-      console.log(this.loginForm);
       
     }
+    citysApl(ev:any){
+      this.cit=[];
+      this.checkoutForm.controls['city'].reset();
+      this.citys.forEach(element => {
+        if (element.state == ev.detail.value ) {
+          this.cit.push(element)
+        }
+      });
+    }
+    paym(ev:any){
+      this.checkoutForm.controls['payme'].reset();
+      console.log(ev);
+      const selectCity=ev.detail.value;
+      this.checkServide.getPaymet(selectCity).subscribe(resp=>{
+        this.payme=resp.data;
+      })
+    }
+    selectAddress(address:number){
+      console.log(address);
+      
+    }
+
 }
